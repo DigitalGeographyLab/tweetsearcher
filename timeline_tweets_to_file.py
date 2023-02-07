c11aea92b84277670d7d72c7114363a99b0bf62f
waeiski <tuomvais@dx8-500-039.geology.helsinki.fi>
2023-02-07 16:21:03 +0200

    add chunk iteration

--- timeline_tweets_to_file.py
+++ timeline_tweets_to_file.py
@@ -53,6 +53,10 @@ import argparse
 import pandas as pd
 import gc
 
+# define function to iterate over users in chunks
+def chunker(sequence, size):
+    return (sequence[pos:pos + size] for pos in range(0, len(sequence), size))
+
 # Set up the argument parser
 ap = argparse.ArgumentParser()
 
@@ -77,16 +81,24 @@ ap.add_argument("-o", "--output", required=True, default='pkl',
                 help="Output file format, valid options are either pkl or csv. "
                 "Default: pkl")
 
-# get bounding box geopackage
+# get output path
 ap.add_argument("-op", "--outpath", required=True,
                 help="Path to output folder. For example: "
                 "~/Data/project/results/")
+
+# get user chunk size
+ap.add_argument("-c", "--chunksize", required=True, default=20,
+                help="The number of users to save into single dataframe."
+                "Default: 20")
 
 # Parse arguments
 args = vars(ap.parse_args())
 
 # get output path
 outpath = args['output']
+
+# get chunk size
+user_chunksize = args['chunksize']
 
 # check if output filetypes are valid
 if args['output'] == 'pkl':
@@ -137,122 +149,145 @@ expansions = ",".join(["attachments.media_keys", "author_id", "entities.mentions
 start_date = args['startdate'].date()
 end_date = args['enddate'].date()
 
-for user in users:
+# get chunks of user list
+for userchunk in chunker(users, user_chunksize):
+    print('users in chunk ' + str(len(userchunk)) + ' and second user id is ' + str(userchunk[1]))
     
-    # form search query per user and rule out retweets, replies and quote tweets
-    search_q = 'from:{} -is:retweet has:geo'.format(user)
+    # get list for user dataframes
+    dflist = []
     
-    # payload rules for v2 api
-    rule = gen_request_parameters(query = search_q,
-                                  results_per_call = config['results_per_call'],
-                                  start_time = start_date.isoformat(),
-                                  end_time = end_date.isoformat(),
-                                  tweet_fields = tweetfields,
-                                  user_fields = userfields,
-                                  media_fields = mediafields,
-                                  place_fields = placefields,
-                                  expansions = expansions,
-                                  stringify = False)
+    # get first and last users
+    fuser = userchunk[0]
+    luser = userchunk[-1]
     
-    # result stream from twitter v2 api
-    rs = ResultStream(request_parameters = rule,
-                      max_results=100000,
-                      max_pages=1,
-                      max_tweets = config['max_tweets'],
-                      **search_creds)
-    
-    # number of reconnection tries
-    tries = 10
-    
-    # while loop to protect against 104 error
-    while True:
-        tries -= 1
+    # loop over users in chunk
+    for user in userchunk:
         
-        # attempt retrieving tweets
-        try:
-            # indicate which day is getting retrieved
-            print('[INFO] - Retrieving tweets between ' + str(start_date) + ' and ' + str(end_date))
+        # form search query per user and rule out retweets, replies and quote tweets
+        search_q = 'from:{} -is:retweet has:geo'.format(user)
         
-            # get json response to list
-            tweets = list(rs.stream())
+        # payload rules for v2 api
+        rule = gen_request_parameters(query = search_q,
+                                      results_per_call = config['results_per_call'],
+                                      start_time = start_date.isoformat(),
+                                      end_time = end_date.isoformat(),
+                                      tweet_fields = tweetfields,
+                                      user_fields = userfields,
+                                      media_fields = mediafields,
+                                      place_fields = placefields,
+                                      expansions = expansions,
+                                      stringify = False)
+        
+        # result stream from twitter v2 api
+        rs = ResultStream(request_parameters = rule,
+                          max_results=100000,
+                          max_pages=1,
+                          max_tweets = config['max_tweets'],
+                          **search_creds)
+        
+        # number of reconnection tries
+        tries = 10
+        
+        # while loop to protect against 104 error
+        while True:
+            tries -= 1
             
-            # add check to adjust wait time
-            if len(tweets) < 500:
+            # attempt retrieving tweets
+            try:
+                # indicate which day is getting retrieved
+                print('[INFO] - Retrieving tweets between ' + str(start_date) + ' and ' + str(end_date))
+            
+                # get json response to list
+                tweets = list(rs.stream())
+                
+                # add check to adjust wait time
+                if len(tweets) < 500:
+                        
+                    # wait for 7 seconds to not hit rate limits if zero tweets
+                    time.sleep(7)
                     
-                # wait for 7 seconds to not hit rate limits if zero tweets
-                time.sleep(7)
-                
-            else:
-                
-                # wait a bit longer
-                time.sleep(18)
-                
-            # break free from while loop
-            break
-        except Exception as err:
-            if tries == 0:
-                raise err
-            else:
-                print('[INFO] - Got connection error, waiting 15 seconds and trying again. ' + str(tries) + ' tries left.')
-                time.sleep(15)
-    
-    # inform how many tweets per user were collected
-    print('[INFO] - Collected ' + str(len(tweets)) + ' tweets from user ' + str(user))
-    
-    # parse results to dataframe
-    if len(tweets) != 0:
-        try:
-            # convert json to dataframe
-            print('[INFO] - Parsing collected tweets of user ' + str(user) + ' from ' + str(start_date) + ' to ' + str(end_date))
-            tweetdf = v2parser(tweets, config['results_per_call'])
+                else:
+                    
+                    # wait a bit longer
+                    time.sleep(18)
+                    
+                # break free from while loop
+                break
+            except Exception as err:
+                if tries == 0:
+                    raise err
+                else:
+                    print('[INFO] - Got connection error, waiting 15 seconds and trying again. ' + str(tries) + ' tries left.')
+                    time.sleep(15)
         
-        except:
-            print('[INFO] - User id ' + str(user) + ' tweets could not be converted to dataframe..')
-            pass
-    else:
-        print('[INFO] - User id ' + str(user) + ' is missing or has no tweets. Moving on...')
-        pass
+        # inform how many tweets per user were collected
+        print('[INFO] - Collected ' + str(len(tweets)) + ' tweets from user ' + str(user))
+        
+        # parse results to dataframe
+        if len(tweets) != 0:
+            try:
+                # convert json to dataframe
+                print('[INFO] - Parsing collected tweets of user ' + str(user) + ' from ' + str(start_date) + ' to ' + str(end_date))
+                tweetdf = v2parser(tweets, config['results_per_call'])
             
+            except:
+                print('[INFO] - User id ' + str(user) + ' tweets could not be converted to dataframe..')
+                pass
+        else:
+            print('[INFO] - User id ' + str(user) + ' is missing or has no tweets. Moving on...')
+            pass
+                
+        
+        # try to order columns semantically
+        try:
+            tweetdf = tweetdf[['id', 'author_id', 'created_at', 'reply_settings', 'conversation_id',
+                               'in_reply_to_user_id', 'text', 'possibly_sensitive',
+                               'lang', 'referenced_tweets', 'referenced_tweets.id', 
+                               'referenced_tweets.author_id', 'referenced_tweets.type',
+                               'public_metrics.retweet_count', 'public_metrics.reply_count',
+                               'public_metrics.like_count', 'public_metrics.quote_count',
+                               'entities.mentions', 'entities.urls', 'entities.hashtags',
+                               'entities.annotations', 'attachments.media_keys',
+                               'attachments.media_types', 'user.description', 'user.verified', 'user.id', 'user.protected',
+                               'user.url', 'user.profile_image_url', 'user.location', 'user.name',
+                               'user.created_at', 'user.username', 'user.public_metrics.followers_count',
+                               'user.public_metrics.following_count', 'user.public_metrics.tweet_count',
+                               'user.public_metrics.listed_count', 'user.entities.description.hashtags',
+                               'user.entities.url.urls', 'user.entities.description.mentions',
+                               'user.entities.description.urls', 'geo.place_id', 'geo.coordinates.type',
+                               'geo.coordinates.coordinates', 'geo.coordinates.x', 'geo.coordinates.y',
+                               'geo.full_name', 'geo.name', 'geo.place_type', 'geo.country',
+                               'geo.country_code', 'geo.type', 'geo.bbox', 'geo.centroid',
+                               'geo.centroid.x', 'geo.centroid.y']]
+        except:
+            pass
+        
+        # append dataframe to dataframe list
+        dflist.append(tweetdf)
+        gc.collect()
+        
+        # delete dataframe variable to cleanse the memory buffer
+        del tweetdf
     
-    # try to order columns semantically
-    try:
-        tweetdf = tweetdf[['id', 'author_id', 'created_at', 'reply_settings', 'conversation_id',
-                           'in_reply_to_user_id', 'text', 'possibly_sensitive',
-                           'lang', 'referenced_tweets', 'referenced_tweets.id', 
-                           'referenced_tweets.author_id', 'referenced_tweets.type',
-                           'public_metrics.retweet_count', 'public_metrics.reply_count',
-                           'public_metrics.like_count', 'public_metrics.quote_count',
-                           'entities.mentions', 'entities.urls', 'entities.hashtags',
-                           'entities.annotations', 'attachments.media_keys',
-                           'attachments.media_types', 'user.description', 'user.verified', 'user.id', 'user.protected',
-                           'user.url', 'user.profile_image_url', 'user.location', 'user.name',
-                           'user.created_at', 'user.username', 'user.public_metrics.followers_count',
-                           'user.public_metrics.following_count', 'user.public_metrics.tweet_count',
-                           'user.public_metrics.listed_count', 'user.entities.description.hashtags',
-                           'user.entities.url.urls', 'user.entities.description.mentions',
-                           'user.entities.description.urls', 'geo.place_id', 'geo.coordinates.type',
-                           'geo.coordinates.coordinates', 'geo.coordinates.x', 'geo.coordinates.y',
-                           'geo.full_name', 'geo.name', 'geo.place_type', 'geo.country',
-                           'geo.country_code', 'geo.type', 'geo.bbox', 'geo.centroid',
-                           'geo.centroid.x', 'geo.centroid.y']]
-    except:
-        pass
+    # concatenate result dataframe
+    results = pd.concat(dflist, ignore_index=True)
     
     # set up file prefix from config
     file_prefix_w_date = config['filename_prefix'] + start_date.isoformat()
-    outpickle = file_prefix_w_date + '_user_' + str(user) + '.pkl'
-    outcsv = file_prefix_w_date + '_user_' + str(user)+ '.csv'
+    userstring = '_from_' + str(fuser) + '_to_' + str(luser)
+    outpickle = file_prefix_w_date + userstring + '.pkl'
+    outcsv = file_prefix_w_date + userstring + '.csv'
     
     # save to file
     if args['output'] == 'pkl':
         # save to pickle
-        tweetdf.to_pickle(outpath + outpickle)
+        results.to_pickle(outpath + outpickle)
     elif args['output'] == 'csv':
         # save to csv
-        tweetdf.to_csv(outpath + outcsv, sep=';', encoding='utf-8')
+        results.to_csv(outpath + outcsv, sep=';', encoding='utf-8')
     
-    # delete dataframe variable to cleanse the memory buffer
-    del tweetdf
+    # free up memory
+    del results, dflist
     gc.collect()
 
 print('[INFO] - ... done!')
